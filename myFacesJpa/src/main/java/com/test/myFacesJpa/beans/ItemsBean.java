53f88832186d5c464b2d5e3af20a275670240acd
chongma <nbmlaw@gmail.com>
2018-01-25 21:18:09 +0100

    working version

--- myFacesJpa/src/main/java/com/test/myFacesJpa/beans/ItemsBean.java
+++ myFacesJpa/src/main/java/com/test/myFacesJpa/beans/ItemsBean.java
@@ -1,17 +1,72 @@
 package com.test.myFacesJpa.beans;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+//import javax.transaction.Transactional;
+
+import org.apache.deltaspike.jpa.api.transaction.Transactional;
+
+import com.test.myFacesJpa.dao.ItemDao;
+import com.test.myFacesJpa.entities.Item;
 
 @Named
 @ViewScoped
 public class ItemsBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Inject
+	private ItemDao itemDao;
+
+	private List<Item> items;
+	private Item item;
+
 	public String onload() {
+		update();
 		return null;
 	}
-	
+
+	private void update() {
+		setItems(itemDao.select());
+	}
+
+	@Transactional
+	public String delete() {
+		itemDao.delete(getItem());
+		update();
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_ERROR, "Deleted item", null));
+		// FacesContext.getCurrentInstance().addMessage(null,
+		// new FacesMessage(FacesMessage.SEVERITY_INFO, "Deleted item", null));
+		return null;
+	}
+
+	@Transactional
+	public String add() {
+		itemDao.create();
+		update();
+		return null;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+
 }
